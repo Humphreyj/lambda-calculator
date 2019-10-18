9a316762eb930ac3e870b0fb59905df0c186d717
HumphreyJ <joshhumphrey1@gmail.com>
2019-10-18 18:55:16 -0500

    Got assistance with my 0 key and the project is complete.

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -2,10 +2,12 @@ import React from "react";
 
 
 const NumberButton = (props) => {
+  console.log(typeof props.num)
   return (
+    
     <div>
       <button 
-      onClick={props.addToInput} className="btn number-btn">{props.num}</button>
+      onClick={props.addToInput} className={props.num == 0 ? `big btn number-btn`: `btn number-btn`}>{props.num}</button>
     </div>
     
     
